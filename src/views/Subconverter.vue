3635bf30d4f0cb76ee8c1bea7c5525051600c680
2275273 <68908577+2275273@users.noreply.github.com>
2024-06-04 02:10:53 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -417,9 +417,9 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-		  "巴莫自用增强后端": "https://nas.239000.xyz:9393",
+		  "巴莫自用增强后端": "https://zhuanhuan.239000.xyz",
      	  "巴莫自用原版后端": "https://nas.239000.xyz:9494",
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://apiurl.v1.mk",
+          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
           "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
           nameless13提供: "https://www.nameless13.com",
@@ -428,7 +428,7 @@ export default {
           "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
-		  {value: "https://nas.239000.xyz:9393/sub?"},
+		  {value: "https://zhuanhuan.239000.xyz/sub?"},
 	  {value: "https://nas.239000.xyz:9393/sub?"},
           {value: "https://apiurl.v1.mk"},
           {value: "https://sub.d1.mk"},
